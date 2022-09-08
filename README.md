42121eafbb72adfede8e3c17d4f4fe12a4e4b8ad
futureshape <alex.baxevanis@gmail.com>
2022-09-08 08:42:57 +0100

    Added Tesco stores

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-Python code to scrape data or access the APIs from store finders for some UK store chains.
+Python code to scrape data or access the APIs from store finders for some UK store chains:
+
+* [GAIL's Bakery](https://gailsbread.co.uk/)
+* [Majestic Wine](https://www.majestic.co.uk/)
+* [Marks & Spencer](https://www.marksandspencer.com/)
+* [Tesco](https://www.tesco.com/)
+* [Waitrose](https://www.waitrose.com/)
 
 Started as a joke to visualise the "posh areas of London" according to [this Reddit post](https://www.reddit.com/r/london/comments/wuwc6c/indicators_of_posh_area_in_london/).
 
---
+++ tesco.py
@@ -0,0 +1,64 @@
+import requests
+import geojson
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+from progress.bar import Bar
+
+ua = UserAgent()
+
+r = requests.get('https://www.tesco.com/store-locator/directory', headers = { 'User-Agent': ua.safari } )
+
+soup = BeautifulSoup(r.text, 'html.parser')
+
+regions = soup.select('.Directory-listLink')
+
+print(f'Found {len(regions)} regions')
+
+features = []
+store_links = []
+
+bar = Bar('Downloading regions', max=len(regions))
+
+for region in regions:
+    region_link = region['href']
+    bar.suffix = '%(index)d/%(max)d ' + region_link
+
+    if '/' in region_link:
+        # regions with only 1 store redirect straight to the store
+        store_links.append(region_link)
+    else:
+        r = requests.get(f'https://www.tesco.com/store-locator/{region_link}', headers = { 'User-Agent': ua.safari } )
+
+        soup = BeautifulSoup(r.text, 'html.parser')
+
+        stores = soup.select('.LocationList .Teaser-button')
+        #print(f'Found {len(stores)} stores')
+
+        for store in stores:
+            store_links.append(store['href'])
+    bar.next()
+
+bar.finish()
+
+print(f'Found {len(store_links)} stores total')
+
+bar = Bar('Downloading stores', max=len(store_links))
+for store_link in store_links:
+    bar.suffix = '%(index)d/%(max)d ' + store_link
+    r = requests.get(f'https://www.tesco.com/store-locator/{store_link}', headers = { 'User-Agent': ua.safari } )
+
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+    longitude = soup.select('meta[itemprop="longitude"]')[0]
+    latitude = soup.select('meta[itemprop="latitude"]')[0]
+
+    store_point = geojson.Point((float(longitude.get('content')), float(latitude.get('content'))))
+
+    features.append(geojson.Feature(geometry=store_point))
+    bar.next()
+
+bar.finish()
+
+all_stores = geojson.FeatureCollection(features)
+with open('tesco.json', 'w') as outfile:
+    geojson.dump(all_stores, outfile)
