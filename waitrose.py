733bbe0e39de9ca29ad51832b6c1652af3f6372f
futureshape <alex.baxevanis@gmail.com>
2022-09-19 19:50:09 +0100

    Waitrose: add store names, pretty print json

--- waitrose.py
+++ waitrose.py
@@ -14,16 +14,17 @@ features = []
 
 for store in stores:
     store_id = store.get('value', None)
+    store_name = store.string
     if store_id != '':
-        print(f'Processing store ID {store_id}')
+        print(f'Processing store {store_id} {store_name}')
         r = requests.get(f'https://www.waitrose.com/content/waitrose/en/bf_home/bf/{store_id}.html')
         if r.status_code != 200:
             continue
         soup = BeautifulSoup(r.text, 'html.parser')
         map_link = soup.select('a.load-branch-map')[0]
         store_point = geojson.Point((float(map_link.get('data-long')), float(map_link.get('data-lat'))))
-        features.append(geojson.Feature(geometry=store_point))
+        features.append(geojson.Feature(geometry=store_point, properties={"name": store_name}))
 
 all_stores = geojson.FeatureCollection(features)
 with open('waitrose.json', 'w') as outfile:
-    geojson.dump(all_stores, outfile)
+    geojson.dump(all_stores, outfile, indent=2)
