425e26ece6042617fe70ee5bc0285cf6bcd1ffc1
Mat Stace <MatStace@users.noreply.github.com>
2023-06-22 11:31:39 +0100

    Added shopName to properties of Greggs stores

--- greggs.py
+++ greggs.py
@@ -11,7 +11,7 @@ features = []
 
 for store in stores:
     store_point = geojson.Point((float(store['address']['longitude']), float(store['address']['latitude'])))
-    features.append(geojson.Feature(geometry=store_point))
+    features.append(geojson.Feature(geometry=store_point, properties={"name": store['shopName']}))
 
 all_stores = geojson.FeatureCollection(features)
 with open('greggs.json', 'w') as outfile:
