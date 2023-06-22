d9713341bd8ff3b54f48eabff594666b55078cf8
Mat Stace <MatStace@users.noreply.github.com>
2023-06-22 11:26:33 +0100

    Added Greggs store locations within 500km of the centre of Great Britain

---
+++ greggs.py
@@ -0,0 +1,18 @@
+import requests
+import geojson
+import json
+
+r = requests.get("https://production-digital.greggs.co.uk/api/v1.0/shops?latitude=54.006994430267156&longitude=-2.5038627362629144&distanceInMeters=500000")
+stores = json.loads(r.text)
+
+print(f'Found {len(stores)} stores')
+
+features = []
+
+for store in stores:
+    store_point = geojson.Point((float(store['address']['longitude']), float(store['address']['latitude'])))
+    features.append(geojson.Feature(geometry=store_point))
+
+all_stores = geojson.FeatureCollection(features)
+with open('greggs.json', 'w') as outfile:
+    geojson.dump(all_stores, outfile, indent=2)
